791055cbbd7230074c39ef20ad4583ff829a8ee9
DavontaGitHub <davonta.navy@gmail.com>
2024-05-05 23:12:13 -0700

    Hour16
    Making screen black on play.

--- DVaughn_Hour16/Assets/ParticleEffect.cs
+++ DVaughn_Hour16/Assets/ParticleEffect.cs
@@ -1,8 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class ParticleEffect : MonoBehaviour
-{
     /* Hello there, the 5 parameters Changed were Emission,Shape,Limit Velocity Over Lifetime,Noise,Lights*/
    
